b1c0b64ee34484f4a5c36308e458b7342c781694
Julio Seña <julio.657.js@gmail.com>
2015-09-01 09:40:10 -0500

    instance bd oracle

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 var mysql = require('mysql'),
     fs = require('fs'),
     jwt = require('jsonwebtoken'),
-    config = require('./config');
+    config = require('./config'),
+    oracledb = require('oracledb');
 
-var cert = fs.readFileSync('public.pem');  // get public key
+//var cert = fs.readFileSync('public.pem');  // get public key
+//
+//var decodeMysql = jwt.verify(config.mysql, cert,{ algorithms: ['RS256'] } );
+//
+//var connection = mysql.createConnection(decodeMysql);
+//
+//    connection.connect();
+//
+//    connection.query('SELECT * FROM test_conection', function(err, rows, fields) {
+//        if (err) throw err;
+//        console.log(rows);
+//    });
+//
+//connection.end();
 
-var decodeMysql = jwt.verify(config.mysql, cert,{ algorithms: ['RS256'] } );
+oracledb.getConnection(
+    {
+        user          : "syndb",
+        password      : "aig2k15",
+        connectString : "10.252.76.115/PLANDAC"
+    },
+    function(err, connection)
+    {
+        if (err) { console.error(err.message); return; }
 
-var connection = mysql.createConnection(decodeMysql);
+        console.log(connection);
 
-    connection.connect();
-
-    connection.query('SELECT * FROM test_conection', function(err, rows, fields) {
-        if (err) throw err;
-        console.log(rows);
+        //connection.execute(
+        //    "SELECT department_id, department_name FROM departments WHERE department_id = :did",
+        //    [180],  // bind value for :did
+        //    function(err, result)
+        //    {
+        //        if (err) { console.error(err.message); return; }
+        //        console.log(result.rows);
+        //    });
     });
-
-connection.end();
