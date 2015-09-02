a680eb767ec6c9599cce16fcdea02ef2676c3a39
Julio Seña <julio.657.js@gmail.com>
2015-09-02 16:34:01 -0500

    create insert or update method

--- crud.js
+++ crud.js
@@ -1,18 +1,16 @@
-
 var mysql = require('mysql'),
     config = require('./config'),
     query = require('./query'),
-    oracledb = require('oracledb'),
-    _ = require('lodash');
+    oracledb = require('oracledb');
 
 
-exports.getLastParamMysql = function (callback) {
+exports.getLastParamMysql = function(callback) {
 
     var connection = mysql.createConnection(config.mysql);
     connection.connect();
 
     connection.query(query.lastParams,
-        function(err, rows, fields) {
+        function(err, rows) {
             connection.end();
 
             if (err) {
@@ -20,42 +18,50 @@ exports.getLastParamMysql = function (callback) {
             }
 
             callback(rows[0]);
-    });
+        });
 
 };
 
-exports.getDataOracle = function (lastFields,callback) {
+exports.getDataOracle = function(lastFields, callback) {
 
-    //var queryConfig = {maxRows: 10,outFormat: oracledb.OBJECT},
-    var queryConfig = {maxRows: 1000},
+    var queryConfig = lastFields.lastNumEmployee ? {
+            maxRows: 1000,
+            outFormat: oracledb.OBJECT
+        } : {
+            maxRows: 1000
+        },
         lastDateUpdate = lastFields.lastDateUpdateEmployee,
         where = '',
         params = {};
 
 
-
-    if(lastDateUpdate){
-        where  = 'WHERE FECHA_DEL_SISTEMAU > :lastDateUpdate';
-        params =
-        {
-            lastDateUpdate: { val: lastDateUpdate, dir: oracledb.BIND_IN, type: oracledb.DATE },
+    if (lastDateUpdate) {
+        where = 'WHERE FECHA_DEL_SISTEMAU > :lastDateUpdate';
+        params = {
+            lastDateUpdate: {
+                val: lastDateUpdate,
+                dir: oracledb.BIND_IN,
+                type: oracledb.DATE
+            }
         }
     }
 
 
     oracledb.getConnection(
         config.oracle,
-        function(err, connection)
-        {
-            if (err) { throw err; }
+        function(err, connection) {
+            if (err) {
+                throw err;
+            }
 
             connection.execute(
-                "SELECT "+config.columms+" FROM PLANDAC.EMPLEADOS "+where,
+                "SELECT " + config.columms + " FROM PLANDAC.EMPLEADOS " + where,
                 params,
                 queryConfig,
-                function(err, result)
-                {
-                    if (err) { throw err; }
+                function(err, result) {
+                    if (err) {
+                        throw err;
+                    }
 
                     callback(result);
                 });
@@ -63,15 +69,20 @@ exports.getDataOracle = function (lastFields,callback) {
 
 };
 
-exports.insertRowMysql = function (rows) {
+exports.insertRowsMysql = function(rows) {
+
+
+    if (rows.length === 0) return;
+
+    console.log('Inserting news employees mysql...');
 
     var connection = mysql.createConnection(config.mysql);
 
     connection.connect();
 
 
-    connection.query("INSERT INTO erp_empleados(??) VALUES ?",[config.columms, rows ], function(err, result) {
-       if(err) throw err;
+    connection.query("INSERT INTO erp_empleados(??) VALUES ?", [config.columms, rows], function(err, result) {
+        if (err) throw err;
     });
 
     connection.end();
@@ -79,8 +90,22 @@ exports.insertRowMysql = function (rows) {
 
 };
 
-exports.updateRowMysql = function (row) {
+exports.insertOrUpdateRowMysql = function(rows) {
 
+    var connection = mysql.createConnection(config.mysql);
+    connection.connect();
 
+    console.log('Init inserting or updating new employee in mysql...');
+
+    if (rows.length === 0) return;
+
+    rows.forEach(function(emp) {
+
+        connection.query('INSERT INTO erp_empleados SET ? ON DUPLICATE KEY UPDATE ?', [emp, emp], function(err, results) {
+
+        });
+
+    });
+    connection.end();
 
 };
