ee56c0416c43f298e1c1db3cc353056189fb5e59
Julio Seña <julio.657.js@gmail.com>
2015-09-03 10:11:50 -0500

    test

--- app.js
+++ app.js
@@ -1,67 +1,23 @@
-var crud = require('./crud'),
-    winston = require('winston');
+var process = require('./server/process'),
+    winston = require('winston'),
+    schedule = require('node-schedule');
 
 winston.add(winston.transports.File, {
     filename: 'node.log'
 });
 
 
-winston.info('Init application...')
-winston.info('Find lastParams in mysql database...');
-crud.getLastParamMysql(function(lastFields) {
 
-    if (!lastFields) {
-        winston.error('Error to connect mysql database');
-        return;
-    }
-
-    winston.info('Find data in oracle database...');
-    crud.getDataOracle(lastFields, function(data) {
-
-        if (!data) {
-            winston.error('Error to connecto oracle database');
-            return;
+schedule.scheduleJob('*/1 * * * *', function(){
+    process.ecxecute(function(isSuccess){
+        if(isSuccess){
+            winston.info('Proccess successfully')
         }
-
-        if (data.rows.length > 0) {
-
-            winston.info('The news data was found...');
-            if (!lastFields.lastNumEmployee) {
-
-                winston.info('Init first insert data...');
-                crud.insertRowsMysql(data.rows);
-            } else {
-
-                winston.info('Init modify data...');
-                crud.insertOrUpdateRowMysql(data.rows);
-
-            }
-
-        } else {
-            winston.info('Not found alter employees');
+        else{
+            winston.warn('Proccess failed')
         }
-
     });
-
 });
-
-
-//
-//
-
-
-
-
-//oracle.connect(connectData, function(err, connection) {
-//    if (err) { console.log("Error connecting to db:", err); return; }
-//
-//    connection.execute("SELECT * FROM PLANDAC.EMPLEADOS", [], function(err, results) {
-//        if (err) { console.log("Error executing query:", err); return; }
-//
-//        console.log(results[0]);
-//        connection.close(); // call only when query is finished executing
-//    });
-//});
 
 
 //var cert = fs.readFileSync('public.pem');  // get public key
